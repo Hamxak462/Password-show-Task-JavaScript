a67e53c4bdaddb69c67f441c3136cc6fd76cafb4
Muhammad Hamza Shah <110331331+Hamxak462@users.noreply.github.com>
2025-01-16 12:04:45 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+         // let password = document.getElementById("input-field");
+        // let eyeicon = document.getElementById("eyeicon");
+
+        // eyeicon.onclick = function(){
+        //     if(password.type == "password"){
+        //         password.type = "text";
+        //         eyeicon.src= 'assets/eye-open.png';
+        //         eyeicon.style.color = 'red'
+        //     }else {
+        //         password.type = "password";
+        //         eyeicon.src= 'assets/eye-hide.png'
+
+        //     }
+        // }
+
+        let password = document.getElementById('input-field');
+        let eyeicon = document.getElementById('eyeicon');
+
+        eyeicon.onclick = function(){
+            if(password.type == 'password'){
+                password.type = 'text';
+                eyeicon.src = 'assets/eye-open.png';
+            }else{
+                password.type = 'password';
+                eyeicon.src= 'assets/eye-hide.png'
+            }
+        }
